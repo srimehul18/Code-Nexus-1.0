9caab86227e3b806846aec16f6f846d622409793
Mehul Srivastava <srimehul18@gmail.com>
2026-04-08 12:20:48 +0530

    Added navbar.jsx

---
+++ myApp/src/components/navbar.jsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+  return (
+    <div style={{ marginBottom: "20px" }}>
+      <h1>🤖 AI Student Toolkit Pro</h1>
+      <p>Your all-in-one AI learning assistant</p>
+    </div>
+  );
+}
